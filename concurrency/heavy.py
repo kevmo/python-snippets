92fd1692e130ee8d939e4381efd4fa1e3fc8177a
Kevin Moore <k.james.moore@gmail.com>
2023-07-16 14:47:41 -0400

    Modularizing to standardize benchmark func.

---
+++ concurrency/heavy.py
@@ -0,0 +1,11 @@
+def heavy(n, id_):
+    for x in range(1, n):
+        for y in range(1, n):
+            x**y 
+    
+    print ("DONE: ", id_)
+
+
+def sequential(n):
+    for i in range(n):
+        heavy(100, i)
--- concurrency/single-thread.py
+++ concurrency/single-thread.py
@@ -1,19 +1,10 @@
 import time
 
-def heavy(n, id_):
-    for x in range(1, n):
-        for y in range(1, n):
-            x**y 
-    
-    print ("DONE: ", id_)
+import heavy
 
-
-def sequential(n):
-    for i in range(n):
-        heavy(100, i)
 
 if __name__ == "__main__":
     start = time.time()
-    sequential(20)
+    heavy.sequential(20)
     end = time.time()
     print("BENCHMARK TIME: ", end - start)
